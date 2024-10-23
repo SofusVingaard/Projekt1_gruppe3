8dd1d8976780c5f24b4989a7c65167635cdefcfb
SofusVingaard <sovi0003@stud.kea.dk>
2024-10-23 10:24:18 +0200

    Åbningstid

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Velkommen til Harry's Salon ");
+        System.out.println("Vores åbningstider er mandag-fredag kl. 10-18");
     }
 }
