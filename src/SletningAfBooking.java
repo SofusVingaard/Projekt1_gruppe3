5124ec7131e3f813e115cd008ec30e57c41418b5
GusViking <guwi0001@stud.kea.dk>
2024-10-23 10:52:49 +0200

    Har lavet en if sætning, men den fungere først når oprettelse af tidspunkt er blevet skrevet

--- src/SletningAfBooking.java
+++ src/SletningAfBooking.java
@@ -1,4 +1,12 @@
 public class SletningAfBooking {
+    public static void main(String[] args) {
 
+        boolean slettet;
+        if (slettet) {
+            System.out.println("Din booking på dette tidspunkt " + tidspunkt +" er slettet");
 
+        }else {
+            System.out.println("Der er ingen booking fundet på dette tidspunkt "+tidspunkt);
+        }
+    }
 }
