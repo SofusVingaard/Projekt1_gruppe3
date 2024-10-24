f18fc231b7850f16413b94ab9dad7edbb9e81c7f
GusViking <guwi0001@stud.kea.dk>
2024-10-24 11:55:37 +0200

    Ændret i SletningAfBooking så der ikke er noget som stopper et run

--- src/SletningAfBooking.java
+++ src/SletningAfBooking.java
@@ -1,24 +1,30 @@
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-/*
+
 public class SletningAfBooking {
+    Scanner keyboard = new Scanner(System.in);
+
+    void bookettid(){
+
+    }
+
     public static void main(String[] args) {
+        String dato;
+        String tidspunkt;
+
+        DateTimeFormatter tidspunktIndtast = DateTimeFormatter.ofPattern("HH:mm");
+
+        DateTimeFormatter datoIndtast = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
         Scanner slettefunktion = new Scanner(System.in);
-        System.out.println("Skriv den dato du har booket");
-        String dato = Scanner.nextLine();
+        System.out.println("Skriv den dato du har booket ved dd-mm-yyyy");
+        dato = slettefunktion.nextLine();
 
         System.out.println("Skriv tidspunkt du har booket");
-        String tidspunkt = Scanner.nextLine();
+        tidspunkt = slettefunktion.nextLine();
 
-    }
-        boolean slettet;
-        if (slettet) {
-            System.out.println("Din booking på dette tidspunkt " + tidspunkt +" er slettet");
-
-        } else {
-            System.out.println("Der er ingen booking fundet på dette tidspunkt "+tidspunkt);
-        }
     }
 }
-\*
- */
+
+
+
