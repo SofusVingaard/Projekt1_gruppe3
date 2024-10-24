2d2f224e287688679c7bc0135f156575698f106d
GusViking <guwi0001@stud.kea.dk>
2024-10-24 12:11:52 +0200

    Har lavet lidt til menuen

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Velkommen til Harry's Salon ");
-        System.out.println("Vores åbningstider er mandag-fredag kl. 10-18");
+        Scanner keyboard = new Scanner(System.in);
+        int tast;
+        String navn;
+
+        while (true) {
+            System.out.println("Velkommen til Harry's Salon ");
+            System.out.println("Vores åbningstider er mandag-fredag kl. 10-18");
+            System.out.println("Tast 1 for book tid");
+            System.out.println("Tast 2 for slet tid");
+            System.out.println("Tast 3 for opretning af kunde");
+            System.out.println("tast 0 for at afslutte");
+
+            tast = keyboard.nextInt();
+
+            if (tast == 0) {
+                System.out.println("Tak for i dag");
+                break;
+            } else if (tast>=4) {
+                System.out.println("Ugyldig valgmulighed, prøv igen");
+                System.out.println();
+            }
+            switch (tast){
+                case 1:
+
+            }
+        }
     }
 }
