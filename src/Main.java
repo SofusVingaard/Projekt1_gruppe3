eeb65b7350e317804fd9718fef831ef183a0fcb4
MuniGlitch <chni0006@stud.kea.dk>
2024-10-24 10:36:49 +0200

    Opret datoer og tid

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,9 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        int tast;
-        String navn;
+        System.out.println("Velkommen til Harry's Salon ");
+        System.out.println("Vores åbningstider er mandag-fredag kl. 10-18");
 
-        while (true) {
-            System.out.println("Velkommen til Harry's Salon ");
-            System.out.println("Vores åbningstider er mandag-fredag kl. 10-18");
-            System.out.println("Tast 1 for book tid");
-            System.out.println("Tast 2 for slet tid");
-            System.out.println("Tast 3 for opretning af kunde");
-            System.out.println("tast 0 for at afslutte");
-
-            tast = keyboard.nextInt();
-
-            if (tast == 0) {
-                System.out.println("Tak for i dag");
-                break;
-            } else if (tast>=4) {
-                System.out.println("Ugyldig valgmulighed, prøv igen");
-                System.out.println();
-            }
-            switch (tast){
-                case 1:
-
-                case 2:
-
-                case 3:
-
-            }
-        }
     }
 }
