6b34ef644959b241b6c297f39d4457798301c0ae
SofusVingaard <sovi0003@stud.kea.dk>
2024-10-25 13:30:00 +0200

    TestingAfMain bruges bare som et test dokument til Sletningafbookning

--- src/TestingAfMain.java
+++ src/TestingAfMain.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class TestingAfMain {
     private static final String filnavn = "src/appointments.txt";  // Navn på filen
-    private static final String booket = "Booket af";    // Tekst for ledig tid
+    private static final String booket = "Booket af";
 
     public static void sletEnTid( String[] args){
         try {
@@ -34,10 +34,9 @@ public class TestingAfMain {
 
             System.out.println("Indtast nummeret på den tid, du vil booke:");
             int valg = scanner.nextInt();
-            scanner.nextLine();  // For at håndtere linjeskift efter nextInt()
+            scanner.nextLine();
 
 
-            // Kontroller om valget er gyldigt
             if (valg < 1 || valg > tiderPåDato.size()) {
                 System.out.println("Ugyldigt valg. Afslutter programmet.");
 
@@ -45,12 +44,12 @@ public class TestingAfMain {
             }
 
             String valgtTid = tiderPåDato.get(valg - 1);
-            String navn=" ";
-            String telefon=" ";
-            String email=" ";
+            String navn="";
+            String telefon="";
+            String email="";
 
             String kundedata = "Ledig tid" +navn+telefon+email;
-            String opdateretTid = valgtTid.replace(booket, kundedata);
+            String opdateretTid = valgtTid.replaceAll(booket, kundedata);
             linjer.set(linjer.indexOf(valgtTid), opdateretTid);
 
 
@@ -63,6 +62,7 @@ public class TestingAfMain {
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+
         }
 
     }
